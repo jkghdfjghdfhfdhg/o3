81b46ea6467c45aaeaa52fcb4c2d790a25499cf0
jkghdfjghdfhfdhg <kurbanov.rustam.mansurovich@gmail.com>
2023-05-03 17:46:39 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,42 @@
+// 1
+
+// function minNumber(num1, num2) {
+//   if (num1 < num2) {
+//     return num1;
+//   } else {
+//     return num2;
+//   }
+// }
+
+// 2
+
+// function powerNumber(num, power) {
+//   return Math.pow(num, power);
+// }
+
+// 3
+
+// function sumArray(array) {
+//   let sum = 0;
+//   for (let num of array) {
+//     sum += num;
+//   }
+//   return sum;
+// }
+
+4
+
+// const sendSMS = function(phoneNumber, message) {
+//   console.log(`SMS has been sent to ${phoneNumber} with message: ${message}`);
+// }
+
+5
+
+// const bankAccount = {
+//   accountNumber: 123456789,
+//   balance: 1000,
+//   sendTransfer: function(accountNumber, amount) {
+//     console.log(`Sending transfer of ${amount} to the account number ${accountNumber}`);
+//     this.balance -= amount; // уменьшаем баланс текущего счета
+//   }
+// }
